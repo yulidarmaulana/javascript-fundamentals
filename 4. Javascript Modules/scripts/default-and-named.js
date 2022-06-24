3b562ec0f32ee12e1b56e0625720f17161787875
yulidarmaulana <maulanayulidar@gmail.com>
2022-06-24 21:18:13 +0700

    belajar javascript modules

---
+++ 4. Javascript Modules/scripts/default-and-named.js
@@ -0,0 +1,8 @@
+export const title = "Belajar Javascript";
+
+const content = "Belajar Javascript Module";
+
+const author = "Yulidar Maulana Ivan Saputra";
+
+export {content};
+export default author;
